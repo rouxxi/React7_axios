1443cc0152d6b0a5716f8d25ca733dbecbfaf1ae
Gwenvael.L <laskar.gwenvael@gmail.com>
2020-10-27 11:48:22 +0100

    add componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import React from 'react';
 import axios from 'axios'
 import QuoteSimpson from './components/QuoteSimpson';
 
-const sampleQuote = {
-  quote: "By chilling my loins I increase the chances of impregnating my wife.",
-  character: "Apu Nahasapeemapetilon",
-  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
-  characterDirection: "Left"
-};
-
 class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      simpsonQuote: sampleQuote
+      simpsonQuote: null
     };
     this.getApiData = this.getApiData.bind(this);
   }
+
+  componentDidMount(){
+    this.getApiData();
+  }
+
   getApiData() {
    axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
     .then(response => {
@@ -29,7 +27,11 @@ render() {
 
   return (
     <div className="App">
-      <QuoteSimpson simpsonQuote={this.state.simpsonQuote}/>
+      {
+        this.state.simpsonQuote
+        ? <QuoteSimpson simpsonQuote={this.state.simpsonQuote}/>
+        : <p> Loading </p>
+    }
       <button type="button" onClick={this.getApiData}>New quote</button>
 
     </div>
